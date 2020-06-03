5e31abf39e69861d3f1ca0fb5a6d5f3533bfeae4
smoothtom <smoothtom@DESKTOP-H07SG8G.localdomain>
2020-06-03 22:58:02 +0200

    files

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-Random Password Generater 
+Welcome to my little Programm which I coded - as you can see its random password generater
 
 # Things that are done 
+
 --> Generate a random Password for you 
     - this string is safed in a file 
  
@@ -9,7 +10,12 @@ Random Password Generater
  --> Date and Time will also be there when the string is generated
 
 # Things that needs to be finished 
+
 --> input which ask the user for what usage its needed
     - passwords that are used for something needs to be safed in a extra file
 
+--> check if you have the file rpg.txt, if not then create this one
+
 --> stuff =)
+
+Have Fun ... 
--- rpg.py
+++ rpg.py
@@ -5,22 +5,39 @@ import string
 import sys
 import time
 import datetime
-import os 
+import os
+from os import path 
+
 print("Welcome to my Random Password Generator!\n")
 
-random = ''.join([random.choice(string.ascii_lowercase + 
-string.ascii_uppercase + string.digits) for x in range(8)])
+first_input = "s"   # s is the key 
+"""x = input("First we have to check if you have the right file in your directory, please enter (s) to continue: ")"""
 
-def query():          
+count = 0   # starts at 0 and goes to 3 
+
+while count < 3:
+    x = input("First we have to check if you have the right file in your directory, please enter (s) to continue: ")
+
+    if x == first_input:
+        break
+    else:
+        print("Please enter again: ")
+        count +=1
+
+random = ''.join([random.choice(string.ascii_lowercase + 
+string.ascii_uppercase + string.digits) for x in range(12)])    # 12 words long generated password
+print("\n")
+def query():            
     print("*********************************************")
     print("Your Generated Password is: ", random)
     print("*********************************************")
     print("\n")
 
-query()
+query() # execute the function above 
 
 text_file = open("rpg.txt", "a+")
-n = text_file.write("Generated Password is: "+ random + " " + "Time: " + time.strftime("%d.%m.%Y um %H:%M:%S Uhr" + "\n"))
+n = text_file.write("Generated Password is: "+ random +
+        " " + "Time: " + time.strftime("%d.%m.%Y um %H:%M:%S Uhr" + "\n"))
 text_file.close()
 
 """while True:
