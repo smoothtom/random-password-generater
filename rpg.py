546624e5272de4ac7778d04b22a2d820b97ec6ed
smoothtom <t-lauth@web.de>
2020-06-06 13:23:04 +0200

    finished project

--- rpg.py
+++ rpg.py
@@ -10,39 +10,29 @@ from os import path
 
 print("Welcome to my Random Password Generator!\n")
 
-first_input = "s"   # s is the key 
-"""x = input("First we have to check if you have the right file in your directory, please enter (s) to continue: ")"""
-
-count = 0   # starts at 0 and goes to 3 
-
-while count < 3:
-    x = input("First we have to check if you have the right file in your directory, please enter (s) to continue: ")
-
-    if x == first_input:
-        break
-    else:
-        print("Please enter again: ")
-        count +=1
-
 random = ''.join([random.choice(string.ascii_lowercase + 
 string.ascii_uppercase + string.digits) for x in range(12)])    # 12 words long generated password
+
 print("\n")
+
 def query():            
     print("*********************************************")
     print("Your Generated Password is: ", random)
     print("*********************************************")
     print("\n")
 
-query() # execute the function above 
+query()
+
+ask_first = input("Do You want to keep this password? (yes or no) : ")
+if ask_first == "yes":
+    ask_user = input("For what usage is it needed? : ")
+    print("Congratulations, your password has been saved in your .txt file")
+else:
+    sys.exit(0)
 
 text_file = open("rpg.txt", "a+")
-n = text_file.write("Generated Password is: "+ random +
-        " " + "Time: " + time.strftime("%d.%m.%Y um %H:%M:%S Uhr" + "\n"))
+n = text_file.write("Generated Password is: "+ random + "\n"
+        "Time: " + time.strftime("%d.%m.%Y um %H:%M:%S Uhr") + "\n"
+        "Usage: " + ask_user + "\n")
 text_file.close()
 
-"""while True:
-    if get_it == New:
-       query()
-       break
-    else:
-        sys.exit(0)"""
